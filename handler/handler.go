f55c2d2eb25685b5413964a2148c40e2b8f3167a
SouL-H <mhmtaydinw@gmail.com>
2022-08-12 16:49:36 +0300

    post/message method added.

---
+++ handler/handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+	"twitch_chat_analysis/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Message(c *gin.Context) {
+	var q models.ResponseModel
+	// Gelen JSON içeriğinin quote yapısına eşlenip eşlenemediği kontrol ediliyor
+	if err := c.ShouldBindJSON(&q); err != nil {
+		log.Print(err)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err}) // Eğer JSON içeriğinde sıkıntı varsa hata mesajı ile birlikte geriye HTTP 400 Bad Request dönüyoruz
+		return
+	}
+	ok := RabbitQueuePush(q)
+
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"msg": "Something went wrong"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"": q})
+
+}
---
+++ service/service.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	hand "twitch_chat_analysis/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ServiceRun() {
+	r := gin.Default()
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, "worked")
+	})
+
+	r.POST("/message", hand.Message)
+
+	r.Run()
+}
